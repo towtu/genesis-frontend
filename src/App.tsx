0f35aecde864890f42b8522977d4e34da10042fe
Richter Anthony Yap <yap.richtera@gmail.com>
2025-02-26 16:48:29 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Login from './components/Login';
 import Register from './components/Register';
 import Dashboard from './components/Dashboard';
 import TodoList from './components/TodoList';
-import Navbar from './components/Navbar';
 import Account from './components/Account';
 import Search from './components/Search';
 import Completed from './components/Completed';
@@ -12,34 +11,48 @@ import Help from './components/Help';
 import Settings from './components/Settings';
 import Important from './components/Important';
 import CalendarView from './components/CalendarPage';
+import MainLayout from './components/MainLayout';
 
 const App: React.FC = () => {
   return (
     <Router>
-      <ConditionalNavbar />
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/todos" element={<TodoList />} /> {/* blanko rani */}
-        <Route path="/account" element={<Account />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/completed" element={<Completed />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/help" element={<Help />} />
-        <Route path="/important" element={<Important />} />
-        <Route path="/calendar" element={<CalendarView />} />
-      </Routes>
+      <AppLayout />
     </Router>
   );
 };
 
-const ConditionalNavbar: React.FC = () => {
+const AppLayout: React.FC = () => {
   const location = useLocation();
-  
-  const showNavbar = location.pathname !== '/register';
 
-  return showNavbar ? <Navbar /> : null;
+  // Define paths where the MainLayout should not be used
+  const noLayoutPaths = ['/login', '/register'];
+
+  // Check if the current path should exclude the MainLayout
+  const useMainLayout = !noLayoutPaths.includes(location.pathname);
+
+  return (
+    <>
+      <Routes>
+        {/* Routes without MainLayout (and without Navbar) */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* Routes with MainLayout (and Navbar in the main body) */}
+        <Route element={useMainLayout ? <MainLayout /> : <></>}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/todos" element={<TodoList />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/completed" element={<Completed />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/help" element={<Help />} />
+          <Route path="/important" element={<Important />} />
+          <Route path="/calendar" element={<CalendarView />} />
+          <Route index element={<Dashboard />} /> {/* Default route */}
+        </Route>
+      </Routes>
+    </>
+  );
 };
 
 export default App;
