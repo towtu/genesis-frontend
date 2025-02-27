36670d9243f61b158b5d19e30e75023071a25670
Richter Anthony Yap <yap.richtera@gmail.com>
2025-02-27 19:43:36 +0800

    Update MainLayout.tsx

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -10,12 +10,12 @@ const MainLayout: React.FC = () => {
       <Sidebar />
 
       {/* Main Content Area */}
-      <div className="flex-1 flex flex-col ml-64"> {/* Adjust margin to match sidebar width */}
+      <div className="flex-1 flex flex-col ml-64">
         {/* Fixed Navbar */}
         <Navbar />
 
         {/* Scrollable Main Content */}
-        <div className="flex-1 overflow-y-auto p-4 mt-16"> {/* Adjust margin to match navbar height */}
+        <div className="flex-1 overflow-y-auto p-4 mt-16">
           <Outlet />
         </div>
       </div>
