561eed76460ee7f493d84d16d59f1a1f534bf4c7
Richter Anthony Yap <yap.richtera@gmail.com>
2025-03-11 17:25:05 +0800

    ..

--- src/components/Completed.tsx
+++ src/components/Completed.tsx
@@ -24,12 +24,7 @@ const Completed: React.FC = () => {
       const response = await api.get('/todo/', {
         params: { completed: true }, // Fetch only completed todos
       });
-      // Add completedDate to each todo if it doesn't exist
-      const todosWithCompletedDate = response.data.map((todo: Todo) => ({
-        ...todo,
-        completedDate: todo.completedDate || new Date().toISOString(), // Simulate completed date
-      }));
-      setCompletedTodos(todosWithCompletedDate);
+      setCompletedTodos(response.data); // No need to modify the data here
     } catch (error) {
       console.error('Failed to fetch completed todos:', error);
     }
